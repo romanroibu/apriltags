eae037d102c2d89c30f2bf84bfe2602fd58ad671
Roman Roibu <romanroibu@gmail.com>
2019-08-08 16:06:24 +0200

    Update build-wheels script to install dev requirements

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -5,6 +5,8 @@ PYTHON_PATH=/opt/python/*cp36*/
 PYTHON_BIN=$PYTHON_PATH/bin/python
 PIP_BIN=$PYTHON_PATH/bin/pip
 
-$PIP_BIN install tox
 cd /io/
+
+$PIP_BIN install tox
+$PIP_BIN install -r requirements_dev.txt
 $PYTHON_BIN -m tox
